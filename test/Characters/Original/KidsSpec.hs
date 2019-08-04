d639cb13fc858b9b2c447f2540b54309fbb83672
Joshua Booth <joshua.n.booth@gmail.com>
2019-08-04 13:15:15 -0700

    Test framework update

--- test/Characters/Original/KidsSpec.hs
+++ test/Characters/Original/KidsSpec.hs
@@ -22,13 +22,14 @@ spec = parallel do
                 it "spends 1 clone" $
                     clonesSpent `shouldBe` 1
                 it "damages target" $
-                    100 - targetHealth `shouldBe` 10 + 5 * clonesSpent
+                    100 - targetHealth `shouldBe` 
+                        10 + 5 * maxClones + 5 * clonesSpent
         useOn Enemy "Rasengan" do
             self $ addStacks "Shadow Clone" maxClones
             act
             clonesSpent   <- getClonesSpent
             targetHealth  <- Ninja.health <$> P.nTarget
-            targetStunned <- Ninja.is (Stun All) <$> P.nTarget
+            targetStunned <- (`is` Stun All) <$> P.nTarget
             return do
                 it "spends 2 clones" $
                     clonesSpent `shouldBe` 2
@@ -37,31 +38,34 @@ spec = parallel do
                 it "stuns target"
                     targetStunned
         useOn Enemy "Shadow Clones" do
+            let targetDmg = 50
             act
             userStacks <- Ninja.numActive "Shadow Clone" <$> P.nUser
-            targetTurn $ damage 50
+            targetTurn $ damage targetDmg
             turns 1
-            targetTurn $ damage 50
+            targetTurn $ damage targetDmg
             turns 1
             targetHealth   <- Ninja.health <$> P.nTarget
             userHealth     <- Ninja.health <$> P.nUser
             userStacks'    <- Ninja.numActive "Shadow Clone" <$> P.nUser
-            let clonesSpent = userStacks' - userStacks
+            let clonesSpent = userStacks - userStacks'
             return do
-                it "grants 5 clones" $
+                it "grants 6 clones" $
                     userStacks `shouldBe` 6
                 it "damages target when damaged" $
                     100 - targetHealth `shouldBe` 5 * clonesSpent
                 it "spends 1 clone per damaged" $
                     clonesSpent `shouldBe` 2
                 it "reduces damage" $
-                    100 - userHealth `shouldBe` (50 - userStacks * 5)
-                                              + (50 - (userStacks - 1) * 5)
+                    100 - userHealth `shouldBe` 
+                        (targetDmg - userStacks * 5)
+                        + (targetDmg - (userStacks - 1) * 5)
 
     describeCharacter "Kiba Inuzuka" \useOn -> do
         useOn Enemy "Two-Headed Wolf" do
+            let targetDmg = 30
             act
-            targetTurn $ damage 30
+            targetTurn $ damage targetDmg
             turns 4
             userHealth   <- Ninja.health <$> P.nUser
             targetHealth <- Ninja.health <$> P.nTarget
@@ -69,6 +73,6 @@ spec = parallel do
                 it "damages target" $
                     100 - targetHealth `shouldBe` 3 * 15
                 it "reduces damage to user" $
-                    100 - userHealth `shouldBe` 15
+                    100 - userHealth `shouldBe` targetDmg - 15
   where
     describeCharacter = describeCategory Original
--- test/TestImport.hs
+++ test/TestImport.hs
@@ -12,6 +12,7 @@ import ClassyPrelude as Import
 import Model.Character as Import (Category(..), Character)
 import Model.Class as Import (Class(..))
 import Model.Effect as Import (Amount(..), Effect(..))
+import Model.Ninja as Import (is)
 import Model.Skill as Import (Target(..))
 import Model.Trap as Import (Trigger(..))
 import Action.Chakra as Import
@@ -26,19 +27,21 @@ import Control.Monad.Trans.State.Strict (StateT, evalStateT)
 import Test.Hspec (SpecWith, describe, it)
 
 import           Core.Util ((!!))
+import qualified Core.Wrapper as Wrapper
+import           Core.Wrapper (Wrapper(Wrapper))
 import qualified Class.Play as P
 import           Class.Play (MonadGame, MonadPlay)
 import           Class.Random (MonadRandom)
 import qualified Model.Context as Context
-import           Model.Context (Context)
+import           Model.Context (Context(Context))
 import           Model.Duration (Duration(..), Turns, sync)
 import qualified Model.Game as Game
-import           Model.Game (Game)
 import qualified Model.Character as Character
 import           Model.Character (Character(Character))
 import qualified Model.Player as Player
 import           Model.Player (Player)
 import qualified Model.Ninja as Ninja
+import           Model.Ninja (Ninja)
 import qualified Model.Skill as Skill
 import qualified Model.Slot as Slot
 import           Model.Slot (Slot)
@@ -51,7 +54,7 @@ import qualified Characters
 
 type TestRun = Target
                -> Text
-               -> ReaderT Context (StateT Game Identity) (SpecWith ())
+               -> ReaderT Context (StateT Wrapper Identity) (SpecWith ())
                -> SpecWith ()
 
 describeCategory :: Category -> Text -> (TestRun -> SpecWith ()) -> SpecWith ()
@@ -66,7 +69,7 @@ useSkill :: Character -> TestRun
 useSkill char target skillName f =
     describe (unpack skillName) case context <$> findSkill skillName char of
         Nothing  -> it "Exists in the database" False
-        Just ctx -> runIdentity $ evalStateT (runReaderT f ctx) $ game char
+        Just ctx -> runIdentity $ evalStateT (runReaderT f ctx) $ testGame char
   where
     findSkill x   = find ((x ==) . Skill.name) . join . Character.skills
     context skill = Context { Context.skill  = skill
@@ -88,11 +91,20 @@ useSkill char target skillName f =
         Everyone   -> 0
         Specific x -> Slot.toInt x
 
-game :: Character -> Game
-game char = Game.new $ char : (character <$> unsafeTail Slot.all)
+testBase :: Wrapper
+testBase = Wrapper 
+    { Wrapper.game = Game.new
+    , Wrapper.ninjas = fromList $ testNinja <$> unsafeTail Slot.all
+    }
 
-character :: Slot -> Character
-character slot = Character
+testGame :: Character -> Wrapper
+testGame char = 
+    testBase { Wrapper.ninjas = charNinja `cons` Wrapper.ninjas testBase }
+  where
+    charNinja = Ninja.new (unsafeHead Slot.all) char
+
+testNinja :: Slot -> Ninja
+testNinja slot = Ninja.new slot $ Character
     { Character.name     = "Ninja " ++ tshow slot
     , Character.bio      = ""
     , Character.skills   = [[Skill.new], [Skill.new], [Skill.new], [Skill.new]]
@@ -105,13 +117,11 @@ wrap player f = do
     whenM ((player /=) . Game.playing <$> P.game) $ Turn.run []
     user  <- P.user
     skill <- P.skill
-    P.modify $ Game.alter Adjust.effects
-    P.trigger user $ OnAction <$> Skill.classes skill
+    P.trigger user $ OnAction <$> toList (Skill.classes skill)
     f
     when (player == Player.A) Execute.addChannels
-    ninjas <- Game.ninjas <$> P.game
-    traverse_ P.launch $ Traps.get user =<< ninjas
-    P.modify $ Game.alter \n -> n { Ninja.triggers = mempty }
+    traverse_ (traverse_ P.launch . Traps.get user) =<< P.ninjas
+    P.modifyAll Adjust.effects
 
 act :: ∀ m. (MonadPlay m, MonadRandom m) => m ()
 act = Turn.process . wrap Player.A $ Execute.effects []
