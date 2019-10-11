ae4639b7c132706a47c0c10aafe4f2cfd81c650c
Joshua Booth <joshua.n.booth@gmail.com>
2019-10-10 18:59:17 -0700

    Gari (R)

--- src/Game/Characters/Reanimated/Adults.hs
+++ src/Game/Characters/Reanimated/Adults.hs
@@ -11,7 +11,7 @@ characters :: [Category -> Character]
 characters =
   [ Character
     "Pakura"
-    "Known as the Hero of the Hidden Sand, Pakura was a successful diplomat until she was betrayed and ambushed by the Hidden Mist Village. Her unique scorch style combines fire and wind elements to create heat orbs that mummify her enemies."
+    "Reanimated by Kabuto, Pakura was a successful diplomat known as the Hero of the Hidden Sand until she was betrayed and ambushed by the Hidden Mist Village. Her unique scorch style combines fire and wind elements to create heat orbs that mummify her enemies."
     [ [ Skill.new
         { Skill.name      = "Scorch Style"
         , Skill.desc      = "Miniature suns orbit Pakura and attack an enemy, dealing 20 affliction damage. For 2 turns, enemies who use skills on Pakura will take 10 affliction damage."
@@ -47,6 +47,52 @@ characters =
         }
       ]
     , [ invuln "Block" "Pakura" [Physical] ]
+    ]
+    75
+  , Character
+    "Gari"
+    "Reanimated by Kabuto, Gari was a member of the Hidden Stone Village's Demolitions Unit. Augmented with explosive force, his taijutsu attacks overwhelm enemies who meet them head-on."
+    [ [ Skill.new
+        { Skill.name      = "Exploding Palm"
+        , Skill.desc      = "Gari strikes an enemy and sets off an explosion at the moment of contact. The next time they use a skill on Gari or his allies, they will take 20 piercing damage. This skill stacks."
+        , Skill.classes   = [Bypassing, Physical, Ranged, Invisible]
+        , Skill.cost      = [Rand]
+        , Skill.cooldown  = 1
+        , Skill.effects   =
+          [ To Enemy do
+                addStack
+                trap 0 OnHarm do
+                    stacks <- targetStacks "Exploding Palm"
+                    pierce (20 * stacks)
+                    removeTrap "Exploding Palm"
+                    remove "Exploding Palm"
+          ]
+        }
+      ]
+    , [ Skill.new
+        { Skill.name      = "Ground Pound"
+        , Skill.desc      = "An explosive shockwave destabilizes the ground around Gari. If an enemy uses a skill on Gari next turn, they will take 25 damage, and Gari will gain 2 turns of 25% damage reduction and restore 15 health each turn."
+        , Skill.classes   = [Chakra, Melee, Invisible]
+        , Skill.cost      = [Blood]
+        , Skill.cooldown  = 2
+        , Skill.effects   =
+          [ To Self $ trapFrom 1 (OnHarmed All) do
+                damage 25
+                self $ apply 2 [Reduce All Percent 25, Heal 15]
+          ]
+        }
+      ]
+    , [ Skill.new
+        { Skill.name      = "Landmine Fist"
+        , Skill.desc      = "Making direct contact with an enemy, Gari generates an explosion inside them that deals 35 piercing damage."
+        , Skill.classes   = [Chakra, Melee]
+        , Skill.cost      = [Blood, Rand]
+        , Skill.cooldown  = 1
+        , Skill.effects   =
+          [ To Enemy $ pierce 35 ]
+        }
+      ]
+    , [ invuln "Dodge" "Gari" [Physical] ]
     ]
     75
   ]
