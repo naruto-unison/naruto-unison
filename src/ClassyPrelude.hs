a753117bce9f825476ac7ad0f55bfb488bf8aadf
Joshua Booth <joshua.n.booth@gmail.com>
2021-01-08 11:11:11 -0800

    Make maximum and minimum strict

--- src/ClassyPrelude.hs
+++ src/ClassyPrelude.hs
@@ -6,41 +6,40 @@
 module ClassyPrelude
   ( module CP
   , module Data.List.NonEmpty
-  , module Prelude
-  , foldl1, foldl1', foldr1, maximum, minimum, maximumBy, minimumBy
+  , foldl1, foldr1, maximum, minimum, maximumBy, minimumBy
   ) where
 
-import "classy-prelude" ClassyPrelude as CP hiding (Handler, head, last, group, groupBy, map, maximum, minimum, maximumBy, minimumBy, init, tail, sum, product)
-import Prelude (map, sum, product)
+import "classy-prelude" ClassyPrelude as CP hiding (Handler, head, last, group, groupBy, maximum, minimum, maximumBy, minimumBy, init, tail)
 import Data.List.NonEmpty (NonEmpty(..), head, last, init, tail, group, groupBy, groupWith, groupAllWith, group1, groupBy1, groupWith1, groupAllWith1)
-import qualified Data.List as List
+import qualified Data.Foldable as F
 
 foldl1 :: ∀ a. (a -> a -> a) -> NonEmpty a -> a
-foldl1 f (x:|xs) = List.foldl f x xs
+foldl1 = F.foldl1
 {-# INLINE foldl1 #-}
 
-foldl1' :: ∀ a. (a -> a -> a) -> NonEmpty a -> a
-foldl1' f (x:|xs) = List.foldl' f x xs
-{-# INLINE foldl1' #-}
-
 foldr1 :: ∀ a. (a -> a -> a) -> NonEmpty a -> a
-foldr1 f (x:|xs) = List.foldr f x xs
+foldr1 = F.foldr1
 {-# INLINE foldr1 #-}
 
+-- This isn't exported; it's a helper function for the following ones.
+foldl1' :: ∀ a. (a -> a -> a) -> NonEmpty a -> a
+foldl1' f (x:|xs) = F.foldl' f x xs
+{-# INLINE foldl1' #-}
+
 maximum :: ∀ a. Ord a => NonEmpty a -> a
-maximum = foldl1 max
+maximum = foldl1' max
 {-# INLINE maximum #-}
 
 minimum :: ∀ a. Ord a => NonEmpty a -> a
-minimum = foldl1 min
+minimum = foldl1' min
 {-# INLINE minimum #-}
 
 maximumBy :: ∀ a. (a -> a -> Ordering) -> NonEmpty a -> a
-maximumBy cmp = foldl1 \x y -> case cmp x y of
+maximumBy cmp = foldl1' \x y -> case cmp x y of
     GT -> x
     _  -> y
 
 minimumBy :: ∀ a. (a -> a -> Ordering) -> NonEmpty a -> a
-minimumBy cmp = foldl1 \x y -> case cmp x y of
+minimumBy cmp = foldl1' \x y -> case cmp x y of
     GT -> y
     _  -> x
