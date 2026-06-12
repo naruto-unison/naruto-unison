62257e3cc0f8a2f16bcce320161503c6335c8837
Joshua Booth <joshua.n.booth@gmail.com>
2026-06-12 10:30:45 -0700

    implement Classed for EnumSet Class and [Class]

--- src/Class/Classed.hs
+++ src/Class/Classed.hs
@@ -1,9 +1,20 @@
 module Class.Classed (Classed(..)) where
 
-import Data.Enum.Set (EnumSet)
+import ClassyPrelude
+
+import           Data.Enum.Set (EnumSet)
+import qualified Data.Enum.Set.Base as Base
 
 import Game.Model.Class (Class(..))
 
 -- | A type with 'Class'es.
 class Classed a where
     classes :: a -> EnumSet Class
+
+instance Classed (Base.EnumSet Word64 Class) where
+    classes = id
+    {-# INLINE classes #-}
+
+instance Classed [Class] where
+    classes = setFromList
+    {-# INLINE classes #-}
